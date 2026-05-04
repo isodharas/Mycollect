bf2c02133e788a77b431bcab583eb9327c7eced0
Dinithi Wijesinghe <Isodharas@gmail.com>
2026-05-04 20:13:53 +0530

    Delete main-project directory

--- main-project/web-dashboard/middleware.ts
+++
@@ -1,4 +0,0 @@
-export { default } from 'next-auth/middleware'
-export const config = {
-  matcher: ['/dashboard/:path*', '/bins/:path*', '/routes/:path*', '/analytics/:path*', '/alerts/:path*', '/reports/:path*']
-}
--- main-project/web-dashboard/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://g7oob1ovd6.execute-api.ap-southeast-2.amazonaws.com/prod',
-  },
-}
-
-export default nextConfig
